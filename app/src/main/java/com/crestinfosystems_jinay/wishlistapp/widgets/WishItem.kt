d2aff95a5b962093f913e2496db2f5e0d0cd9efc
mr-j-shah <shahjinay02@gmail.com>
2024-01-18 16:46:22 +0530

    Icon Change

--- app/src/main/java/com/crestinfosystems_jinay/wishlistapp/widgets/WishItem.kt
+++ app/src/main/java/com/crestinfosystems_jinay/wishlistapp/widgets/WishItem.kt
@@ -1,28 +1,20 @@
 package com.crestinfosystems_jinay.wishlistapp.widgets
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material3.Icon
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.crestinfosystems_jinay.wishlistapp.DataModel.Wish
-import com.crestinfosystems_jinay.wishlistapp.R
 import com.crestinfosystems_jinay.wishlistapp.utils.ColorUtils
-import com.crestinfosystems_jinay.wishlistapp.utils.ComposeUtils
 
 @Composable
 fun wishItem(wish: Wish, onClick: () -> Unit) {
